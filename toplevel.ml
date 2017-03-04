6f9bac5bd544dbe7f87c5617ea3e1cebcbab04d5
Viet Le <vietlq85@gmail.com>
2017-03-04 11:42:46 +0000

    Spacing

--- toplevel.ml
+++ toplevel.ml
@@ -1,24 +1,24 @@
 let rec main_loop stream =
-	match Stream.peek stream with
-	| None -> ()
-	| Some (Token.Kwd ';') ->
-		Stream.junk stream;
-		main_loop stream
-	| Some token ->
-		begin
-			try match token with
-				| Token.Def ->
-					ignore(Parser.parse_definition stream);
-					print_endline "Parsed a function definition.";
-				| Token.Extern ->
-					ignore(Parser.parse_extern stream);
-					print_endline "Parsed an extern.";
-				| _ ->
-					ignore(Parser.parse_toplevel stream);
-					print_endline "Parsed a top-level expr.";
-			with Stream.Error s ->
-				Stream.junk stream;
-				print_endline s;
-		end;
-		print_string "ready> "; flush stdout;
-		main_loop stream
+  match Stream.peek stream with
+  | None -> ()
+  | Some (Token.Kwd ';') ->
+  	Stream.junk stream;
+  	main_loop stream
+  | Some token ->
+  	begin
+  		try match token with
+  			| Token.Def ->
+  				ignore(Parser.parse_definition stream);
+  				print_endline "Parsed a function definition.";
+  			| Token.Extern ->
+  				ignore(Parser.parse_extern stream);
+  				print_endline "Parsed an extern.";
+  			| _ ->
+  				ignore(Parser.parse_toplevel stream);
+  				print_endline "Parsed a top-level expr.";
+  		with Stream.Error s ->
+  			Stream.junk stream;
+  			print_endline s;
+  	end;
+  	print_string "ready> "; flush stdout;
+  	main_loop stream
--- toy.ml
+++ toy.ml
@@ -1,14 +1,14 @@
 let main () =
-	Hashtbl.add Parser.binop_precedence '<' 10;
-	Hashtbl.add Parser.binop_precedence '+' 20;
-	Hashtbl.add Parser.binop_precedence '-' 20;
-	Hashtbl.add Parser.binop_precedence '*' 40;
-	Hashtbl.add Parser.binop_precedence '/' 40;
+  Hashtbl.add Parser.binop_precedence '<' 10;
+  Hashtbl.add Parser.binop_precedence '+' 20;
+  Hashtbl.add Parser.binop_precedence '-' 20;
+  Hashtbl.add Parser.binop_precedence '*' 40;
+  Hashtbl.add Parser.binop_precedence '/' 40;
 
-	print_string "ready> "; flush stdout;
-	let stream = Lexer.lex (Stream.of_channel stdin) in
+  print_string "ready> "; flush stdout;
+  let stream = Lexer.lex (Stream.of_channel stdin) in
 
-	Toplevel.main_loop stream;
+  Toplevel.main_loop stream;
 ;;
 
 main()
